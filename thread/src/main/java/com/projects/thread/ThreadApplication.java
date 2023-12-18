0743be96938c3e61605693062990b117d39edf4d
ysbc1247 <ystc1247@khu.ac.kr>
2023-12-19 04:46:48 +0900

    [add] thread count logging

--- thread/src/main/java/com/projects/thread/ThreadApplication.java
+++ thread/src/main/java/com/projects/thread/ThreadApplication.java
@@ -1,13 +1,25 @@
 package com.projects.thread;
+import java.util.concurrent.TimeUnit;
 
+import net.sf.jsqlparser.statement.select.KSQLJoinWindow;
+import net.sf.jsqlparser.statement.select.KSQLWindow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @SpringBootApplication
+@EnableAsync
 public class ThreadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThreadApplication.class, args);
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        // Log the thread count every 10 seconds
+        executor.scheduleAtFixedRate(ThreadMonitoring::logThreadCount, 0, 5, TimeUnit.SECONDS);
     }
 
 }
